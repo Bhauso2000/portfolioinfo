c505f865b137fc4696e544cf64503ff113797802
Bhauso Shinde <meghanath2905@gmail.com>
2025-05-04 17:13:57 +0530

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Bhauso Shinde",
+  profile: '/profilephoto.jpg',
+  designation: "Software Developer",
+  description: "I am a full stack developer with expertise in React, React Native, Node.js, Prisma, and MongoDB. I build modern web and mobile applications with scalable back-end architectures and intuitive front-end experiences. My focus is on writing clean, efficient code and leveraging the latest technologies to deliver high-performance, production-ready solutions.",
+  email: 'bhausoshinde2000@gmail.com',
+  phone: '+918275117308',
+  address: '2/861, Jay Bhavani Ichalkaranji',
+  github: '#',
+  facebook: '#',
+  linkedIn: '#',
+  twitter: '#',
+  stackOverflow: '#',
+  leetcode: "https://leetcode.com/u/Ahmed_Ewees/",
+  devUsername: "",
+  resume: "/svg/resume/resume.pdf"
+}
