902e9e0057de5b24cab74f2cd0effe456c564976
Bhauso Shinde <meghanath2905@gmail.com>
2025-05-04 18:03:46 +0530

    go version1

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -15,7 +15,7 @@ export default async function Home() {
       <>
         <HeroSection />
         <AboutSection />
-        {/* <Experience /> */}
+        <Experience />
         <Skills />
         <Projects />
         <Education />
